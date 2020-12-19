257302efdb2af1da307a6e6a9c391417b8909b8c
Zack Scholl <zack.scholl@gmail.com>
2020-12-19 23:03:48 +0700

    add stereo

--- blndr.lua
+++ blndr.lua
@@ -26,7 +26,7 @@ shift=0
 monitor_linein=1
 rate=1.0
 bpm=90
-speeds={1,1}
+speeds={1,1,1,1}
 pan=0.5
 multipliers={1/3,2/3,1,1+1/3,1+2/3,2}
 mi=3
@@ -40,37 +40,47 @@ function init()
   audio.level_adc_cut(1)
 
   -- add parameters
-  params:add_control("level","level",controlspec.new(0,1,"lin",0,0,"",1/100))
-  params:set_action("level",function(x)
-    for i=1,2 do
-      softcut.level(i,x)
-    end
+  params:add_control("delay level","delay level",controlspec.new(0,1,"lin",0,0,"",1/100))
+  params:set_action("delay level",function(x)
+    softcut.level(1,x)
+    softcut.level(3,x)
   end)
   params:add_control("feedback","feedback",controlspec.new(0,1,"lin",0,.5,"",1/100))
   params:set_action("feedback",function(x)
-    for i=1,2 do
+    for i=1,4 do
       softcut.rec_level(i,x)
       softcut.pre_level(i,x)
     end
   end)
+  params:add_control("spin level","spin level",controlspec.new(0,1,"lin",0,0,"",1/100))
+  params:set_action("spin level",function(x)
+    softcut.level(2,x)
+    softcut.level(4,x)
+  end)
   params:add_control("spin","spin",controlspec.new(0,1,"lin",0,0,"",1/100))
   params:set_action("spin",function(x)
     if x==0 then
-      for i=1,2 do
+      for i=1,4 do
         softcut.pan(i,0)
       end
     else
-      for i=1,2 do
+      for i=1,4 do
         if reverse_mode==0 then
-          softcut.pan(i,((i*2)-3)*pan)
+          softcut.pan(1,-1*pan)
+          softcut.pan(2,pan)
+          softcut.pan(3,pan)
+          softcut.pan(4,-1*pan)
         else
-          softcut.pan(i,((i*2)-3)*x)
+          softcut.pan(1,-1*x)
+          softcut.pan(2,x)
+          softcut.pan(3,x)
+          softcut.pan(4,-1*x)
         end
       end
     end
   end)
-  filter_resonance=controlspec.new(0,1,'lin',0,0,'')
-  filter_freq=controlspec.new(20,20000,'exp',0,20000,'Hz')
+  filter_resonance=controlspec.new(0,.9,'lin',0,0,'')
+  filter_freq=controlspec.new(20,20000,'exp',0,5000,'Hz')
   params:add {
       type='control',
       id='filter_frequency',
@@ -78,7 +88,7 @@ function init()
       controlspec=filter_freq,
       formatter=Formatters.format_freq,
       action=function(value)
-        for i=1,2 do
+        for i=1,4 do
           softcut.post_filter_fc(i,value)
         end
       end
@@ -89,18 +99,18 @@ function init()
     name='filter resonance',
     controlspec=filter_resonance,
     action=function(value)
-      for i=1,2 do
+      for i=1,4 do
         softcut.post_filter_rq(i,1-value)
       end
     end
   }
   softcut.buffer_clear() 
-  for i=1,2 do
+  for i=1,4 do
     softcut.enable(i,1)
     softcut.buffer(i,i)
     softcut.loop(i,1)
-    softcut.loop_start(i,1)
-    softcut.loop_end(i,1+60/bpm)
+    softcut.loop_start(i,1+i*10)
+    softcut.loop_end(i,1+60/bpm+i*10)
     softcut.position(i,1)
     softcut.play(i,1)
     softcut.rec_level(i,params:get("feedback"))
@@ -109,7 +119,6 @@ function init()
     softcut.rate(i,1)
     softcut.rate_slew_time(i,60/bpm*1.5)
     softcut.pan_slew_time(i,60/bpm*1.5)
-    softcut.level(i,params:get("level"))
     softcut.post_filter_dry(i,0.0)
     softcut.post_filter_lp(i,1.0)
     softcut.post_filter_rq(i,1.0)
@@ -118,15 +127,23 @@ function init()
     softcut.pre_filter_lp(i,1.0)
     softcut.pre_filter_rq(i,1.0)
     softcut.pre_filter_fc(i,20100)
-    softcut.pan(i,((i*2)-3)*pan)
   end
+  softcut.level(1,params:get("delay level"))
+  softcut.level(2,params:get("spin"))
+  softcut.level(3,params:get("delay level"))
+  softcut.level(4,params:get("spin"))
+  softcut.pan(1,-1*pan)
+  softcut.pan(2,pan)
+  softcut.pan(3,pan)
+  softcut.pan(4,-1*pan)
 
 
   -- send input audio to channel 1
   softcut.level_input_cut(1,1,1.0)
-  softcut.level_input_cut(2,1,1.0)
+  softcut.level_input_cut(2,3,1.0)
   -- send output of channel 1 to channel 2
   softcut.level_cut_cut(1,2,1)
+  softcut.level_cut_cut(3,4,1)
   
 
   m=metro.init()
@@ -138,21 +155,23 @@ function init()
       count=3-count
       speeds[count]=speeds[count]*-1
       softcut.rate(count,speeds[count])
-      softcut.level(count,params:get("level"))
-      softcut.level(3-count,0)
       softcut.level_slew_time(count,60/(bpm*multipliers[mi])*0.25)
       softcut.level_slew_time(3-count,60/(bpm*multipliers[mi])*0.02)
+      speeds[count+2]=speeds[count+2]*-1
+      softcut.rate(count+2,speeds[count+2])
+      softcut.level_slew_time(count+2,60/(bpm*multipliers[mi])*0.25)
+      softcut.level_slew_time(3-count+2,60/(bpm*multipliers[mi])*0.02)
       do return end
     end
     
     -- event should run every 2 beats at current speed
     -- m.time = 60/(bpm*multipliers[mi])/math.abs(speeds[1])*2
     -- time   = 60s/min/(beats / minute)/(current speed)*(2 beats)
-    local speeds_sel={0.25,0.25,0.5,0.75,1}
-    for i=1,2 do
+    local speeds_sel={0.25,0.25,0.5,0.75,1,2}
+    for i=1,4 do
       local new_speed=speeds[i]
       -- revert approximately every 8 beats
-      if math.random()<0.25 then
+      if math.random()<0.1 then
         new_speed=1
         -- if speeds[i] < 0 then
         --   new_speed = -1
@@ -173,7 +192,7 @@ function init()
           end
         end
         -- reverse pans
-        if i==1 then
+        if i==1 or i==3 then
           pan=pan*-1
           softcut.pan(i,pan)
         else
@@ -203,8 +222,8 @@ end
 function update_parms()
   if bpm~=state_lastbpm then
     state_lastbpm=bpm
-    for i=1,2 do
-      softcut.loop_end(i,1+60/(bpm*multipliers[mi]))
+    for i=1,4 do
+      softcut.loop_end(i,1+60/(bpm*multipliers[mi])+i*10)
       softcut.level_slew_time(i,60/(bpm*multipliers[mi]))
       softcut.rate_slew_time(i,60/(bpm*multipliers[mi])*0.5)
       softcut.pan_slew_time(i,60/(bpm*multipliers[mi]))
@@ -225,6 +244,7 @@ function enc(n,d)
     end
   elseif n==3 then
     params:set("spin",params:get("spin")+d*0.01)
+    params:set("spin level",params:get("spin level")+d*0.01)
   end
   redraw()
 end
@@ -242,46 +262,54 @@ function key(n,z)
       m.time=60/(bpm*multipliers[mi])/math.abs(speeds[1])*2
       -- send input audio to channel 1
       softcut.level_input_cut(1,1,1.0)
-      softcut.level_input_cut(2,1,1.0)
+      softcut.level_input_cut(2,3,1.0)
       -- send output of channel 1 to channel 2
       softcut.level_cut_cut(1,2,1)
-      for i=1,2 do
-        softcut.pan(i,((i*2)-3)*pan)
+      softcut.level_cut_cut(3,4,1)
+      for i=1,4 do
         softcut.level_slew_time(i,60/(bpm*multipliers[mi]))
         softcut.rate_slew_time(i,60/(bpm*multipliers[mi])*0.5)
       end
+      softcut.pan(1,-1*pan)
+      softcut.pan(2,pan)
+      softcut.pan(3,pan)
+      softcut.pan(4,-1*pan)
     else
       -- reverse mode
       softcut.buffer_clear()
       m.time=60/(bpm*multipliers[mi])
       -- clear output from 1 to 2
       softcut.level_cut_cut(1,2,0)
-      for i=1,2 do
+      softcut.level_cut_cut(3,4,0)
+      for i=1,4 do
         -- each channel listens to itself
         softcut.level_input_cut(i,i,1.0)
         softcut.level_slew_time(i,60/(bpm*multipliers[mi])*0.25)
         softcut.rate_slew_time(i,60/(bpm*multipliers[mi])*0.25)
         softcut.pan_slew_time(i,60/(bpm*multipliers[mi])*0.25)
-        softcut.pan(i,((i*2)-3)*params:get("spin"))
       end
+      softcut.pan(1,-1*pan*params:get("spin"))
+      softcut.pan(2,pan*params:get("spin"))
+      softcut.pan(3,pan*params:get("spin"))
+      softcut.pan(4,-1*pan*params:get("spin"))
     end
-    for i=1,2 do
+    for i=1,4 do
       speeds[i]=1
       softcut.rate(i,1)
     end
   elseif n==3 and z==1 then
     if mi<6 then
       mi=mi+1
-      for i=1,2 do
-        softcut.loop_end(i,1+60/(bpm*multipliers[mi]))
+      for i=1,4 do
+        softcut.loop_end(i,1+60/(bpm*multipliers[mi])+i*10)
       end
       m.time=60/(bpm*multipliers[mi])/math.abs(speeds[1])*2
     end
   elseif n==2 and z==1 then
     if mi>1 then
       mi=mi-1
-      for i=1,2 do
-        softcut.loop_end(i,1+60/(bpm*multipliers[mi]))
+      for i=1,4 do
+        softcut.loop_end(i,1+60/(bpm*multipliers[mi])+i*10)
       end
       m.time=60/(bpm*multipliers[mi])/math.abs(speeds[1])*2
     end
